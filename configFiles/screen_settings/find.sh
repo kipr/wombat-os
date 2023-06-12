8e5cfa95919332a24ecfc9fa5a75e53b33df4d72
Erin Harrington <erinharrington12@yahoo.com>
2023-06-12 13:18:31 -0500

    Adjusted find.sh system command

--- configFiles/screen_settings/find.sh
+++ configFiles/screen_settings/find.sh
@@ -3,12 +3,12 @@
 if grep -q "InvertX" /etc/X11/xorg.conf.d/99-calibration.conf #if using inverted configs, need default
 then 
         sudo cp /home/kipr/wombat-os/screen_settings/Default/99-calibration.conf /etc/X11/xorg.conf.d/99-calibration.conf
-        system("xrandr --output HDMI-1 --rotate normal")
+        xrandr --output HDMI-1 --rotate normal
         echo "Inverted settings found"
 
 else #if using default configs, need inverted
         sudo cp /home/kipr/wombat-os/screen_settings/Inverted/99-calibration.conf /etc/X11/xorg.conf.d/99-calibration.conf
-        system("xrandr --output HDMI-1 --rotate inverted")
+        xrandr --output HDMI-1 --rotate inverted
         echo "Default settings found"
 
 fi
