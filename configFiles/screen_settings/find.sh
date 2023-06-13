a884524f8a3cb6de2bcbf62478ae471a675cfe53
Erin Harrington <erinharrington12@yahoo.com>
2023-06-13 09:22:59 -0500

    Adjusted find.sh to find and change current 99-calibrations

--- configFiles/screen_settings/find.sh
+++ configFiles/screen_settings/find.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-if grep -q "InvertX" /etc/X11/xorg.conf.d/99-calibration.conf #if using inverted configs, need default
+if grep -q "InvertY" /etc/X11/xorg.conf.d/99-calibration.conf #if using inverted configs, need default
 then 
         sudo cp /home/kipr/wombat-os/screen_settings/Default/99-calibration.conf /etc/X11/xorg.conf.d/99-calibration.conf
-        xrandr --output HDMI-1 --rotate normal
         echo "Inverted settings found"
 
 else #if using default configs, need inverted
         sudo cp /home/kipr/wombat-os/screen_settings/Inverted/99-calibration.conf /etc/X11/xorg.conf.d/99-calibration.conf
-        xrandr --output HDMI-1 --rotate inverted
         echo "Default settings found"
 
 fi
-sleep 2
+sleep 1
 
 sudo reboot -h now
