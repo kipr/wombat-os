1f8d697e3ea6b0c89afd9ddd1639a52f02ff929f
Erin <erinharrington12@yahoo.com>
2023-06-16 10:30:02 -0500

    Added correct file path in find.sh

--- configFiles/screen_settings/find.sh
+++ configFiles/screen_settings/find.sh
@@ -2,11 +2,11 @@
 
 if grep -q "InvertY" /etc/X11/xorg.conf.d/99-calibration.conf #if using inverted configs, need default
 then 
-        sudo cp /home/kipr/wombat-os/screen_settings/Default/99-calibration.conf /etc/X11/xorg.conf.d/99-calibration.conf
+        sudo cp /home/kipr/wombat-os/configFiles/screen_settings/Default/99-calibration.conf /etc/X11/xorg.conf.d/99-calibration.conf
         echo "Inverted settings found"
 
 else #if using default configs, need inverted
-        sudo cp /home/kipr/wombat-os/screen_settings/Inverted/99-calibration.conf /etc/X11/xorg.conf.d/99-calibration.conf
+        sudo cp /home/kipr/wombat-os/configFiles/screen_settings/Inverted/99-calibration.conf /etc/X11/xorg.conf.d/99-calibration.conf
         echo "Default settings found"
 
 fi
