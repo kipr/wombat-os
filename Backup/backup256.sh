2e8661c1e9ee383136c0dbbc4479d88bdfd4d924
Erin <erinharrington12@yahoo.com>
2024-01-12 12:33:23 -0600

    changed backup256.sh to catch more special characters in names

--- Backup/backup256.sh
+++ Backup/backup256.sh
@@ -1,12 +1,17 @@
+
 #!/bin/bash
 
 sudo mount /dev/sd?? /mnt
-echo -e "Copying Files to USB Drive...\n" 
+echo -e "Copying Files to USB Drive...\n"
 
-cp -r /home/root/Documents/KISS/* /mnt
+sudo chmod -R 777 /home/root/Documents/KISS
+sudo find /home/root/Documents/KISS -depth \( -name "* " -o -name "*)" -o -name "*?"  \) -execdir rename 's/[\s\)]+$//' {} \;
+cp -r /home/root/Documents/KISS/* "/mnt/"
 echo -e "Removing unwanted files... \n"
 
 cd /mnt &&  rm -r bin && rm -r data && rm -r lib && rm -r src && rm -r include
+echo "$PWD"
+. ./modify_json.sh
 sudo umount /dev/sd??
 
 
