04c9dfcd207486f8c44a698e976100bd9ade10ad
tcorbly <45633570+tcorbly@users.noreply.github.com>
2024-01-13 10:22:35 -0700

    Update create3_server.sh to have all stops

--- configFiles/create3_server.sh
+++ configFiles/create3_server.sh
@@ -2,11 +2,13 @@
 
 case "$1" in
         start)
+                sudo podman stop -a
                 sudo podman container prune -f
                 sudo podman run -dt --rm --net=host --env IP=192.168.125.1 --name create3_server docker.io/kipradmin/create3_docker
                 ;;
         stop)
                 sudo podman stop create3_server
+                sudo podman stop -a
                 ;;
         *)
                 echo "Usage: $0 {start|stop}"
