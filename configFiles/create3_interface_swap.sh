6f7c6175500affb8a0262717edbfe24186e42d15
Tim Corbly <tcorbly@kipr.org>
2024-01-25 14:44:19 -0700

    Add direct paths

--- configFiles/create3_interface_swap.sh
+++ configFiles/create3_interface_swap.sh
@@ -2,12 +2,12 @@
 
 case "$1" in
   eth)
-    sudo cat create3_eth_ip.txt > create3_server_ip.txt || { echo "Failed to copy create3_eth_ip.txt"; exit 1;}
-    sudo cat interfaces_eth.txt > /etc/network/interfaces || { echo "Failed to copy interfaces_eth.txt"; exit 1;}
+    sudo cat /home/kipr/wombat-os/configFiles/create3_eth_ip.txt > /home/kipr/wombat-os/configFiles/create3_server_ip.txt || { echo "Failed to copy create3_eth_ip.txt"; exit 1;}
+    sudo cat /home/kipr/wombat-os/configFiles/interfaces_eth.txt > /etc/network/interfaces || { echo "Failed to copy interfaces_eth.txt"; exit 1;}
     ;;
   wifi)
-    sudo cat create3_wifi_ip.txt > create3_server_ip.txt || { echo "Failed to copy create3_wifi_ip.txt"; exit 1;}
-    sudo cat interfaces_wifi.txt > /etc/network/interfaces || { echo "Failed to copy interfaces_wifi.txt"; exit 1;}
+    sudo cat /home/kipr/wombat-os/configFiles/create3_wifi_ip.txt > create3_server_ip.txt || { echo "Failed to copy create3_wifi_ip.txt"; exit 1;}
+    sudo cat /home/kipr/wombat-os/configFiles/interfaces_wifi.txt > /etc/network/interfaces || { echo "Failed to copy interfaces_wifi.txt"; exit 1;}
     ;;
   *)
     echo "Usage: $0 {eth|wifi}"
