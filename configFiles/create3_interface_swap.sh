5da863883697e1b767e94657df7682bc61a0551d
Tim Corbly <tcorbly@kipr.org>
2024-01-25 14:46:58 -0700

    Last direct path switched

--- configFiles/create3_interface_swap.sh
+++ configFiles/create3_interface_swap.sh
@@ -6,7 +6,7 @@ case "$1" in
     sudo cat /home/kipr/wombat-os/configFiles/interfaces_eth.txt > /etc/network/interfaces || { echo "Failed to copy interfaces_eth.txt"; exit 1;}
     ;;
   wifi)
-    sudo cat /home/kipr/wombat-os/configFiles/create3_wifi_ip.txt > create3_server_ip.txt || { echo "Failed to copy create3_wifi_ip.txt"; exit 1;}
+    sudo cat /home/kipr/wombat-os/configFiles/create3_wifi_ip.txt > /home/kipr/wombat-os/configFiles/create3_server_ip.txt || { echo "Failed to copy create3_wifi_ip.txt"; exit 1;}
     sudo cat /home/kipr/wombat-os/configFiles/interfaces_wifi.txt > /etc/network/interfaces || { echo "Failed to copy interfaces_wifi.txt"; exit 1;}
     ;;
   *)
