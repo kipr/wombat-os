183b9ec3b916ec7c2b942f047ebb30ae919a412c
Erin <erinharrington12@yahoo.com>
2026-05-18 12:31:27 -0500

    Moved package cleanup to make space for update

--- updateFiles/wombat_update.sh
+++ updateFiles/wombat_update.sh
@@ -24,10 +24,29 @@ echo "..."
 
 # Check if board_fw_version.txt exists
 if [ ! -f /usr/share/kipr/board_fw_version.txt ]; then
-    echo "Your version is too old to update. Please reflash your SD card."
-    exit 1
+	echo "Your version is too old to update. Please reflash your SD card."
+	exit 1
 fi
 
+# Cleanup for space
+echo "Cleaning up space..."
+sudo apt-get remove --purge \
+	libboost1.74-dev \
+	pypy \
+	firmware-atheros \
+	firmware-libertas \
+	firmware-misc-nonfree \
+	containernetworking-plugins \
+	vlc-l10n \
+	realvnc-vnc-server \
+	pocketsphinx-en-us \
+	git \
+	podman \
+	libxcb-doc -y
+
+sudo apt autoremove --purge -y
+
+sudo rm -rf /var/lib/containers/* /var/lib/apt/lists/* /var/cache/apt/archives/* /home/kipr/Bookshelf /usr/share/doc/* /usr/share/man/* /usr/share/locale/*
 
 ###############################
 #
@@ -43,13 +62,13 @@ sudo chmod u+x $HOME/updateMe.sh
 # Change to configFiles directory and copy board_fw_version.txt to kipr share directory
 cd $HOME/wombat-os/configFiles
 if [ ! -d /usr/share/kipr ]; then
-    sudo mkdir /usr/share/kipr
+	sudo mkdir /usr/share/kipr
 fi
 
 sudo cp board_fw_version.txt /usr/share/kipr/
 sudo cp board_copyright_year.txt /usr/share/kipr/
 sudo cp journald.conf /etc/systemd/journald.conf
-sudo cat interfaces_wifi.txt > /etc/network/interfaces
+sudo cat interfaces_wifi.txt >/etc/network/interfaces
 
 # Copy new Wombat picture over old one
 sudo cp $HOME/wombat-os/wombat.jpg /usr/share/rpd-wallpaper/wombat.jpg
@@ -74,7 +93,6 @@ sudo rm /home/kipr/wombat_launcher.sh
 
 #remount root filesystem as read write
 sudo mount -o remount,rw /
-
 
 ###############################
 #
@@ -113,26 +131,6 @@ sudo dpkg -i pkgs/installs/libgpiod2_1.6.2-1_arm64.deb
 sudo dpkg -i pkgs/installs/libgpiod-dev_1.6.2-1_arm64.deb
 sudo dpkg -i pkgs/installs/gpiod_1.6.2-1_arm64.deb
 
-# Cleanup
-echo "Cleaning up space..."
-sudo apt-get remove --purge \
-  libboost1.74-dev \
-  pypy \
-  firmware-atheros \
-  firmware-libertas \
-  firmware-misc-nonfree \
-  containernetworking-plugins \
-  vlc-l10n \
-  realvnc-vnc-server \
-  pocketsphinx-en-us \
-  git \
-  podman \
-  libxcb-doc -y
-
-sudo apt autoremove --purge -y
-
-sudo rm -rf /var/lib/containers/* /var/lib/apt/lists/* /var/cache/apt/archives/* /home/kipr/Bookshelf /usr/share/doc/* /usr/share/man/* /usr/share/locale/*
-
 cd $HOME
 
 ###############################
@@ -150,20 +148,20 @@ sudo cp $HOME/wombat-os/configFiles/udhcpd /etc/default/udhcpd
 echo "Removing Create 3 Capn'Proto files if present..."
 capnProtoFiles=$(find / -type f -iname "*capn*" 2>/dev/null)
 if [ -n "$capnProtoFiles" ]; then
-  echo "Found files to delete:"
-  echo "$capnProtoFiles"
-  find / -type f -iname "*capn*" -exec rm -f {} \; 2>/dev/null
-  echo "Files deleted."
+	echo "Found files to delete:"
+	echo "$capnProtoFiles"
+	find / -type f -iname "*capn*" -exec rm -f {} \; 2>/dev/null
+	echo "Files deleted."
 else
-  echo "No files matching '*capn*' were found."
+	echo "No files matching '*capn*' were found."
 fi
 
 #Remove Create 3 deb file if present
 echo "Removing Create 3 .deb file if present..."
 create3Deb=$(find /home/kipr -type f -iname "create3-0.1.0-Linux.deb" 2>/dev/null)
-if [ -n "$create3Deb"  ]; then
-  echo "Removing Create 3 deb file"
-  sudo rm /home/kipr/create3-0.1.0-Linux.deb
+if [ -n "$create3Deb" ]; then
+	echo "Removing Create 3 deb file"
+	sudo rm /home/kipr/create3-0.1.0-Linux.deb
 fi
 
 #Adding Default Programs
@@ -171,9 +169,9 @@ echo "Checking for Default User"
 TARGET="/home/kipr/wombat-os/updateFiles/files/Wombat Factory Test"
 CP_TARGET="/home/kipr/Documents/KISS/Default User/"
 if [ ! -d "$CP_TARGET" ]; then
-    mkdir "$CP_TARGET" || echo "Failed to make Default User"
-else 
-    echo "Default User already exists"
+	mkdir "$CP_TARGET" || echo "Failed to make Default User"
+else
+	echo "Default User already exists"
 fi
 echo "Adding Default Programs"
 sudo cp -R "$TARGET" "$CP_TARGET" || echo "Failed to copy Default Programs"
@@ -194,11 +192,17 @@ echo "Finished Wombat Update #$FW_VERSION"
 # Remove old wombat-os if it exists
 cd /home/kipr
 if [ -d "wombat-os-old" ]; then
-  sudo rm -R wombat-os-old || { echo "Failed to remove old wombat-os"; exit 1; }
+	sudo rm -R wombat-os-old || {
+		echo "Failed to remove old wombat-os"
+		exit 1
+	}
 fi
 
 sudo chown -R kipr:kipr /home/kipr/Documents
 
 echo "Rebooting..."
 
-echo "Update Complete" && sudo reboot || { echo "Could not reboot"; exit 1; }
+echo "Update Complete" && sudo reboot || {
+	echo "Could not reboot"
+	exit 1
+}
