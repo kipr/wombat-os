fd70eada7b347cd76c0af77b9b1d88cfb6e0fa53
twells46 <173561638+twells46@users.noreply.github.com>
2026-06-19 08:32:34 -0500

    Prevent `balance.sh` from trying to bring up incorrect SSID

--- flashFiles/wallaby_get_serial.sh
+++ flashFiles/wallaby_get_serial.sh
@@ -1,17 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
 
-VCMD1='i2cget -y 1 0x50 0x40'
-VCMD2='i2cget -y 1 0x50 0x41'
-VCMD3='i2cget -y 1 0x50 0x42'
-VCMD4='i2cget -y 1 0x50 0x43'
-
-V1=$(eval $VCMD1)
-V2=$(eval $VCMD2)
-V3=$(eval $VCMD3)
-V4=$(eval $VCMD4)
-
-printf '%s' "\x$(printf %x ${V1})" | sed 's/^...//'
-printf '%s' "\x$(printf %x ${V2})" | sed 's/^...//'
-printf '%s' "\x$(printf %x ${V3})" | sed 's/^...//'
-printf '%s' "\x$(printf %x ${V4})" | sed 's/^...//'
-
+for reg in 0x40 0x41 0x42 0x43; do
+    v=$(i2cget -y 1 0x50 "$reg")
+    printf '%b' "\\x${v#0x}"
+done
